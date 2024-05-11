5d6c250359308c131c1e0ef8a3af49565f90790d
Mandeep Kumar <88364285+itsdeep904@users.noreply.github.com>
2024-05-11 22:33:45 +0530

    dffd
    nfdm,

--- Birthday.js
+++ Birthday.js
@@ -46,10 +46,29 @@ function showcandle(){
     var helloDiv = document.querySelector('.candle_hidden');
     var _candle_title = document.querySelector('.candle_title');
     var _candle_btn = document.querySelector('.candle_btn');
+    var _cakecut_title = document.querySelector('.cakecut_title');
+    var _cutcake_btn = document.querySelector('.cutcake_btn');
     if (!helloDiv.classList.contains('d-none')) {
         helloDiv.classList.add('d-none');
         _candle_title.classList.add('d-none');
         _candle_btn.classList.add('d-none');
+        _candle_btn.classList.add('d-none');
+        _cakecut_title.classList.remove('d-none');
+        _cutcake_btn.classList.remove('d-none');
         helloDiv.style.zIndex = '0';
+    }
+}
+
+
+function cakecut() {
+    var helloDiv = document.querySelector('.cut_cake');
+    var _cutcake_btn = document.querySelector('.cutcake_btn');
+    var _cakecut_title = document.querySelector('.cakecut_title');
+    var _Wishh4 = document.querySelector('.Wishh4');
+    if (helloDiv.classList.contains('d-none')) {
+        helloDiv.classList.remove('d-none');
+        _cutcake_btn.classList.add('d-none');
+        _cakecut_title.classList.add('d-none');
+        _Wishh4.classList.remove('d-none');
     }
 }
